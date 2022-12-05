03cc5fd33b55cc0b80cc9792812b45786d31cfd2
Robert <robcina@gmail.com>
2022-12-04 19:25:26 -0500

    Add Marginalia package

--- myinit.el
+++ myinit.el
@@ -290,6 +290,21 @@
   ;; (setq consult-project-function (lambda (_) (locate-dominating-file "." ".git")))
 )
 
+;; Enable rich annotations using the Marginalia package
+(use-package marginalia
+  :straight t
+  ;; Either bind `marginalia-cycle' globally or only in the minibuffer
+  :bind (("M-A" . marginalia-cycle)
+         :map minibuffer-local-map
+         ("M-A" . marginalia-cycle))
+
+  ;; The :init configuration is always executed (Not lazy!)
+  :init
+
+  ;; Must be in the :init section of use-package such that the mode gets
+  ;; enabled right away. Note that this forces loading the package.
+  (marginalia-mode))
+
     (use-package amx :straight t)
 
     (use-package counsel :straight t)
