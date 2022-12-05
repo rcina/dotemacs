72323554d35f32c0ab83f79903cabc074d74acc0
Robert <robcina@gmail.com>
2022-12-04 19:20:51 -0500

    Add orderless package

--- myinit.el
+++ myinit.el
@@ -162,6 +162,17 @@
   ;; Enable recursive minibuffers
   (setq enable-recursive-minibuffers t))
 
+;; Optionally use the `orderless' completion style.
+(use-package orderless
+  :straight t
+  :init
+  ;; Configure a custom style dispatcher (see the Consult wiki)
+  ;; (setq orderless-style-dispatchers '(+orderless-dispatch)
+  ;;       orderless-component-separator #'orderless-escapable-split-on-space)
+  (setq completion-styles '(orderless basic)
+        completion-category-defaults nil
+        completion-category-overrides '((file (styles partial-completion)))))
+
     (use-package amx :straight t)
 
     (use-package counsel :straight t)
