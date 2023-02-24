c7e2e3a93f0c6a219acc65db291d51b101533762
Robert <robcina@gmail.com>
2023-02-24 15:25:06 -0500

    Fix yasnippet keybinding

--- myinit.el
+++ myinit.el
@@ -1095,7 +1095,7 @@ capture was not aborted."
 (yas-global-mode 1)
 ;; Bind `SPC' to `yas-expand' when snippet expansion available (it
 ;; will still call `self-insert-command' otherwise).
-(define-key yas-minor-mode-map (kbd "SPC") yas-maybe-expand)
+(define-key yas-minor-mode-map (kbd "C-c j") yas-maybe-expand)
 ;; Bind `C-c y' to `yas-expand' ONLY.
 (define-key yas-minor-mode-map (kbd "C-c y") #'yas-expand)
 
