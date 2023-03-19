7eb0c1a94ebf0c09ad64ecbed4d0e55d112863ec
Robert <robcina@gmail.com>
2023-03-19 16:35:03 -0400

    Add drag-stuff mode

--- myinit.el
+++ myinit.el
@@ -121,11 +121,11 @@
   :defer t)
 (load-theme 'doom-palenight t)
 
-(use-package powerline
-  :straight t
-  :config
-  (require 'powerline)
-  (powerline-center-theme))
+    (use-package powerline
+      :straight t
+      :config
+      (require 'powerline)
+      (powerline-center-theme))
 
 ;; Enable vertico
 (use-package vertico
@@ -492,6 +492,12 @@
   :straight t
   :config
   (require 'inform))
+
+(use-package drag-stuff
+  :straight t
+  :config
+  (require 'drag-stuff)
+  (drag-stuff-mode t))
 
 (use-package which-key
   :straight t
@@ -1074,12 +1080,12 @@ capture was not aborted."
   (define-key dired-mode-map (kbd "C-c t") 'dired-hide-dotfiles-mode)
   )
 
-(use-package dired+
-  :straight t
-  :config
-  (require 'dired+))
+    (use-package dired+
+      :straight t
+      :config
+      (require 'dired+))
 
-(setq hippie-expand-try-functions-list
+    (setq hippie-expand-try-functions-list
           '(try-expand-dabbrev-visible
             try-expand-dabbrev
             try-expand-dabbrev-all-buffers
@@ -1099,9 +1105,9 @@ capture was not aborted."
 ;; Bind `C-c y' to `yas-expand' ONLY.
 (define-key yas-minor-mode-map (kbd "C-c y") #'yas-expand)
 
-(use-package yasnippet-snippets :straight t)
+    (use-package yasnippet-snippets :straight t)
 
-(add-hook 'text-mode-hook 'turn-on-auto-fill)
+    (add-hook 'text-mode-hook 'turn-on-auto-fill)
 
 (use-package flycheck-aspell
   :straight t
@@ -1140,11 +1146,11 @@ capture was not aborted."
   (when (bound-and-true-p flycheck-mode)
    (flycheck-buffer)))
 
-(use-package multiple-cursors :straight t)
-(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+    (use-package multiple-cursors :straight t)
+    (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+    (global-set-key (kbd "C->") 'mc/mark-next-like-this)
+    (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+    (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 
 (use-package aggressive-indent :straight t)
 
@@ -1219,34 +1225,34 @@ capture was not aborted."
   :config
   (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc)))
 
-(use-package expand-region
-  :straight t
-  :config
-  (defun ha/expand-region (lines)
-    "Prefix-oriented wrapper around Magnar's `er/expand-region'.
+    (use-package expand-region
+      :straight t
+      :config
+      (defun ha/expand-region (lines)
+        "Prefix-oriented wrapper around Magnar's `er/expand-region'.
 
-     Call with LINES equal to 1 (given no prefix), it expands the
-     region as normal.  When LINES given a positive number, selects
-     the current line and number of lines specified.  When LINES is a
-     negative number, selects the current line and the previous lines
-     specified.  Select the current line if the LINES prefix is zero."
-    (interactive "p")
-    (cond ((= lines 1)   (er/expand-region 1))
-          ((< lines 0)   (ha/expand-previous-line-as-region lines))
-          (t             (ha/expand-next-line-as-region (1+ lines)))))
+         Call with LINES equal to 1 (given no prefix), it expands the
+         region as normal.  When LINES given a positive number, selects
+         the current line and number of lines specified.  When LINES is a
+         negative number, selects the current line and the previous lines
+         specified.  Select the current line if the LINES prefix is zero."
+        (interactive "p")
+        (cond ((= lines 1)   (er/expand-region 1))
+              ((< lines 0)   (ha/expand-previous-line-as-region lines))
+              (t             (ha/expand-next-line-as-region (1+ lines)))))
 
-  (defun ha/expand-next-line-as-region (lines)
-    (message "lines = %d" lines)
-    (beginning-of-line)
-    (set-mark (point))
-    (end-of-line lines))
+      (defun ha/expand-next-line-as-region (lines)
+        (message "lines = %d" lines)
+        (beginning-of-line)
+        (set-mark (point))
+        (end-of-line lines))
 
-  (defun ha/expand-previous-line-as-region (lines)
-    (end-of-line)
-    (set-mark (point))
-    (beginning-of-line (1+ lines)))
+      (defun ha/expand-previous-line-as-region (lines)
+        (end-of-line)
+        (set-mark (point))
+        (beginning-of-line (1+ lines)))
 
-  :bind ("C-=" . ha/expand-region))
+      :bind ("C-=" . ha/expand-region))
 
 (use-package hungry-delete
   :straight t
@@ -1467,8 +1473,8 @@ capture was not aborted."
 
   (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode)))
 
-(add-hook 'js2-mode-hook
-          (lambda () (flycheck-select-checker "javascript-eslint")))
+ (add-hook 'js2-mode-hook
+           (lambda () (flycheck-select-checker "javascript-eslint")))
 
 (use-package js-comint
   :straight t)
@@ -1649,54 +1655,54 @@ instead."
 ;;  :straight t
 ;;  :after magit)
 
-(use-package git-gutter
-  :straight t
-  :init
-  (global-git-gutter-mode +1))
+  (use-package git-gutter
+    :straight t
+    :init
+    (global-git-gutter-mode +1))
 
-(custom-set-variables
- '(git-gutter:update-interval 2))
+  (custom-set-variables
+   '(git-gutter:update-interval 2))
 
 (straight-use-package 'git-timemachine
     :straight t)
 
-(defhydra hydra-git-gutter (:body-pre (git-gutter-mode 1)
-			    :hint nil)
-  "
-Git gutter:
-  _j_: next hunk        _s_tage hunk     _q_uit
-  _k_: previous hunk    _r_evert hunk    _Q_uit and deactivate git-gutter
-  ^ ^                   _p_opup hunk
-  _h_: first hunk
-  _l_: last hunk        set start _R_evision
-"
-  ("j" git-gutter:next-hunk)
-  ("k" git-gutter:previous-hunk)
-  ("h" (progn (goto-char (point-min))
-	      (git-gutter:next-hunk 1)))
-  ("l" (progn (goto-char (point-min))
-	      (git-gutter:previous-hunk 1)))
-  ("s" git-gutter:stage-hunk)
-  ("r" git-gutter:revert-hunk)
-  ("p" git-gutter:popup-hunk)
-  ("R" git-gutter:set-start-revision)
-  ("q" nil :color blue)
-  ("Q" (progn (git-gutter-mode -1)
-	      ;; git-gutter-fringe doesn't seem to
-	      ;; clear the markup right away
-	      (sit-for 0.1)
-	      (git-gutter:clear))
-       :color blue))
+        (defhydra hydra-git-gutter (:body-pre (git-gutter-mode 1)
+				  :hint nil)
+	"
+      Git gutter:
+	_j_: next hunk        _s_tage hunk     _q_uit
+	_k_: previous hunk    _r_evert hunk    _Q_uit and deactivate git-gutter
+	^ ^                   _p_opup hunk
+	_h_: first hunk
+	_l_: last hunk        set start _R_evision
+      "
+	("j" git-gutter:next-hunk)
+	("k" git-gutter:previous-hunk)
+	("h" (progn (goto-char (point-min))
+		    (git-gutter:next-hunk 1)))
+	("l" (progn (goto-char (point-min))
+		    (git-gutter:previous-hunk 1)))
+	("s" git-gutter:stage-hunk)
+	("r" git-gutter:revert-hunk)
+	("p" git-gutter:popup-hunk)
+	("R" git-gutter:set-start-revision)
+	("q" nil :color blue)
+	("Q" (progn (git-gutter-mode -1)
+		    ;; git-gutter-fringe doesn't seem to
+		    ;; clear the markup right away
+		    (sit-for 0.1)
+		    (git-gutter:clear))
+	     :color blue))
 
-(use-package markdown-mode
-  :straight t
-  :commands (markdown-mode gfm-mode)
-  :mode (("README\\.md\\'" . gfm-mode)
-         ("\\.md\\'" . markdown-mode)
-         ("\\.markdown\\'" . markdown-mode))
-  :init (setq markdown-command "multimarkdown"))
+  (use-package markdown-mode
+    :straight t
+    :commands (markdown-mode gfm-mode)
+    :mode (("README\\.md\\'" . gfm-mode)
+           ("\\.md\\'" . markdown-mode)
+           ("\\.markdown\\'" . markdown-mode))
+    :init (setq markdown-command "multimarkdown"))
 
-;; AucTeX settings - almost no changes
+ ;; AucTeX settings - almost no changes
 (use-package tex-site
   :ensure auctex)
 
