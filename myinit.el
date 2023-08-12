31400a9abdabefb637f485f8462de45c24575f0a
Robert <robcina@gmail.com>
2023-08-12 10:06:43 -0400

    Change corfu configuration

--- myinit.el
+++ myinit.el
@@ -364,8 +364,22 @@
   ;; See also `corfu-excluded-modes'.
   :init
   (global-corfu-mode)
-  (setq tab-always-indent 'complete)
 )
+;; A few more useful configurations...
+(use-package emacs
+  :straight t
+  :init
+  ;; TAB cycle if there are only few candidates
+  (setq completion-cycle-threshold 3)
+
+  ;; Emacs 28: Hide commands in M-x which do not apply to the current mode.
+  ;; Corfu commands are hidden, since they are not supposed to be used via M-x.
+  ;; (setq read-extended-command-predicate
+  ;;       #'command-completion-default-include-p)
+
+  ;; Enable indentation+completion using the TAB key.
+  ;; `completion-at-point' is often bound to M-TAB.
+  (setq tab-always-indent 'complete))
 
 (use-package embark
   :straight t
