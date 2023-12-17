28c4c6655c0aca369e7b99cc94760242f6f89c73
Robert <robcina@gmail.com>
2023-12-17 15:10:50 -0500

    Add more keybindings to avy-mode

--- myinit.el
+++ myinit.el
@@ -1469,8 +1469,12 @@ With a prefix ARG, remove start location."
 (global-set-key (kbd "C-|") 'avy-goto-char)
 (global-set-key (kbd "C-'") 'avy-goto-char-2)
 (global-set-key (kbd "M-g f") 'avy-goto-line)
+(global-set-key (kbd "M-g M-c") 'avy-copy-line)
+(global-set-key (kbd "M-g M-m") 'avy-move-line)
 (global-set-key (kbd "M-g w") 'avy-goto-word-1)
 (global-set-key (kbd "M-g e") 'avy-goto-word-0)
+(global-set-key (kbd "M-g M-j") 'avy-goto-char-timer)
+(setq avy-timeout 1.0)
 
 (use-package neotree
   :straight t)
