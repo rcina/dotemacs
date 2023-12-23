eb9a2b6402422f08b0558ca3b04846f11f699dc8
Robert <robcina@gmail.com>
2023-12-23 13:43:55 -0500

    Add ability to embed youtube links in org-mode documents

--- myinit.el
+++ myinit.el
@@ -1279,6 +1279,27 @@ With a prefix ARG, remove start location."
 (use-package org-web-tools
   :straight t)
 
+(defvar yt-iframe-format
+  ;; You may want to change your width and height.
+  (concat "<iframe width=\"440\""
+          " height=\"335\""
+          " src=\"https://www.youtube.com/embed/%s\""
+          " frameborder=\"0\""
+          " allowfullscreen>%s</iframe>"))
+
+(org-add-link-type
+ "yt"
+ (lambda (handle)
+   (browse-url
+    (concat "https://www.youtube.com/embed/"
+            handle)))
+ (lambda (path desc backend)
+   (cl-case backend
+     (html (format yt-iframe-format
+                   path (or desc "")))
+     (latex (format "\href{%s}{%s}"
+                    path (or desc "video"))))))
+
 (straight-use-package 'company-prescient)
 
 ;; Use coreutils ls
