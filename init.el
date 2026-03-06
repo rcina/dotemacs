c01e78704952888eae352d343ee72fde08c9c003
Robert <robcina@gmail.com>
2026-03-06 16:20:17 -0500

    Fix straight repo and minor adjustments

--- init.el
+++ init.el
@@ -8,24 +8,28 @@
 
 
 ;; Straight.el package manager bootstrap code
-
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
+      (bootstrap-version 6))
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
         (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         ;; FIXED URL: radian-software, not raxod502
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
          'silent 'inhibit-cookies)
       (goto-char (point-max))
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 
 
+
+
 ;; install use-package macro
 (straight-use-package 'use-package)
+(setq straight-use-package-by-default t)
 (require 'use-package)
+
 
 (straight-use-package 'org)
 (require 'org)
